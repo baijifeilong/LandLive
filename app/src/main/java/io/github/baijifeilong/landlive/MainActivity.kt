73bc1dbed40379d931140732a0ac3b4d33c9ec9e
BaiJiFeiLong <baijifeilong@gmail.com>
2018-12-02 20:36:22 +0800

    Fix some bugs; Update layout

--- app/src/main/java/io/github/baijifeilong/landlive/MainActivity.kt
+++ app/src/main/java/io/github/baijifeilong/landlive/MainActivity.kt
@@ -1,24 +1,30 @@
 package io.github.baijifeilong.landlive
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.util.TypedValue
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.RelativeLayout
 import android.widget.TextView
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
-import org.jetbrains.anko.sdk27.coroutines.onItemClick
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var hell: View
     private lateinit var heaven: View
+    private var freedom = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +32,8 @@ class MainActivity : AppCompatActivity() {
 
         val inputs = mutableListOf<Char>()
 
-        val text = Scanner(resources.openRawResource(R.raw.playlist)).useDelimiter("\\Z").next()
-        text.split("\n").forEach { println(it) }
+        val text = Scanner(resources.openRawResource(R.raw.playlist)).useDelimiter("\\A").next()
+        val playlist = text.split("\n").map { it.split(" ") }.map { it.first() to it.last() }
         setContentView(verticalLayout {
             relativeLayout {
                 hell = this
@@ -41,8 +47,8 @@ class MainActivity : AppCompatActivity() {
                                     onClick {
                                         inputs.add((it as Button).text[0])
                                         if (inputs.size >= 4 && inputs.reversed().subList(0, 4).joinToString("") == "4698") {
-                                            hell.visibility = View.GONE
-                                            heaven.visibility = View.VISIBLE
+                                            freedom = true
+                                            recreate()
                                         }
                                     }
                                 }
@@ -55,29 +61,55 @@ class MainActivity : AppCompatActivity() {
                     bottomMargin = 120
                 }
             }.lparams(matchParent, matchParent)
-            listView {
+            addView(RecyclerView(this@MainActivity).apply {
                 heaven = this
-                visibility = View.GONE
-                adapter = object : ArrayAdapter<Pair<String, String>>(this@MainActivity, android.R.layout.simple_list_item_1, android.R.id.text1) {
-                    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-                        return super.getView(position, convertView, parent).apply {
-                            this.find<TextView>(android.R.id.text1).text = getItem(position).first
+                val landscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+                layoutManager = StaggeredGridLayoutManager(if (landscape) 4 else 2, StaggeredGridLayoutManager.VERTICAL)
+                adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+                    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+                        return object : RecyclerView.ViewHolder(LayoutInflater.from(this@MainActivity).inflate(android.R.layout.simple_list_item_1, parent, false).apply {
+                            with(TypedValue()) {
+                                context.theme.resolveAttribute(android.R.attr.selectableItemBackground, this, true)
+                                backgroundResource = resourceId
+                            }
+                        }) {}.apply {
+                            itemView.onClick {
+                                val pos = (heaven as RecyclerView).getChildLayoutPosition(itemView)
+                                startActivity(Intent(Intent.ACTION_VIEW).apply {
+                                    setDataAndType(Uri.parse(playlist.get(pos).second), "application/x-mpegURL")
+                                    putExtra("title", "LandLive(${pos + 1}) ${playlist.get(pos).first}")
+                                })
+                            }
                         }
                     }
-                }.apply {
-                    text.split("\n").forEach {
-                        val parts = it.split(" ")
-                        add(parts.first() to parts.last())
+
+                    override fun getItemCount(): Int {
+                        return playlist.size
                     }
-                    onItemClick { parent, view, position, id ->
-                        startActivity(Intent(Intent.ACTION_VIEW).apply {
-                            setDataAndType(Uri.parse(getItem(position).second), "application/x-mpegURL")
-                            putExtra("title", getItem(position).first)
-                        })
+
+                    @SuppressLint("SetTextI18n")
+                    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+                        holder.itemView.findViewById<TextView>(android.R.id.text1).text = "${position + 1}. ${playlist[position].first}"
                     }
                 }
-            }
+            })
         })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState!!.putBoolean("freedom", freedom)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        freedom = savedInstanceState!!.getBoolean("freedom", false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hell.visibility = if (freedom) View.GONE else View.VISIBLE
+        heaven.visibility = if (!freedom) View.GONE else View.VISIBLE
     }
 
     override fun onBackPressed() {
